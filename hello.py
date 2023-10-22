1647b867e0a9665bba8a9a260322b557adbdbcb4
Youraa <you@example.com>
2023-10-22 20:55:08 +0700

    ke dua

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
-
 import secrets
-secret_key = secrets.token_hex(16)
 from functools import wraps
+
+secret_key = secrets.token_hex(16)
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
+            flash('Anda perlu masuk untuk mengakses halaman ini.')
             return redirect(url_for('hasil'))
         return f(*args, **kwargs)
     return decorated_function
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
@@ -21,35 +21,25 @@ def hasil():
     if 'user' in session:
         user = session['user']
         return render_template('result.html', user=user)
-
-
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-@login_required
-
-def penjumlahan():
-
+def login():
     if request.method == 'POST':
-        user = request.form['angka1']
+        user = request.form['user']
         session['user'] = user
-       
-        flash('berhasil logn')
-    return render_template('result.html', user =  user)
-
+        flash('Berhasil login')
+        return redirect(url_for('hasil'))
+    return render_template('login.html')
 
 @app.route('/buatakun', methods=['GET', 'POST'])
 def buat():
     return render_template('buat.html')
 
-
 @app.route('/keluar')
 def keluar():
-    # Hapus informasi sesi
     session.pop('user', None)
     return redirect(url_for('hasil'))
 
 if __name__ == '__main__':
-    print("apk berjalan")
     app.run(debug=True)
-    
